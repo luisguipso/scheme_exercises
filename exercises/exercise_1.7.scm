6562ce4ed14839974200fcc9283964bbd6691195
luis.gomes <luis.gomes@objective.com.br>
2023-11-23 15:25:01 -0300

    exercise 1.7

--- exercises/exercise_1.7.scm
+++ exercises/exercise_1.7.scm
@@ -8,18 +8,24 @@ stop when the change is a very small fraction of the guess.
 Design a square-root procedure that uses this kind of end
 test. Does this work beer for small and large numbers?
 
+;Solution:
 (define (square x)
   (* x x))
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+(define (good-enough? previous-guess guess)
+  (< (abs (/ (- guess previous-guess) guess)) 0.00000000001))
 (define (average x y)
   (/ (+ x y) 2))
 (define (improve guess x) 
   (average guess (/ x guess)))
 (define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+  (if (good-enough? guess (improve guess x))
       guess
 	  (sqrt-iter (improve guess x) x)))	
 (define (sqrt x) 
   (sqrt-iter 1.0 x))
-(sqrt 2)
+
+
+;for small numbers testing if the difference between the square of the guess and the x would fail fast, because if a number was smaller than the precision we have difined as 0.001
+;for large number the problem was rounding errors.
+
+;more detailed explanation: https://sicp-solutions.net/post/sicp-solution-exercise-1-7/
