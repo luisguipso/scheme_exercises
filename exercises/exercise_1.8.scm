ed7929b66a87e800ed0d40a93ba97ca61832f1ba
luis.gomes <luis.gomes@objective.com.br>
2023-11-24 08:14:24 -0300

    exercise 1.8 -add exercise text.

--- exercises/exercise_1.8.scm
+++ exercises/exercise_1.8.scm
@@ -1,3 +1,9 @@
+Newton’s method for cube roots is based on the fact that if y is an approximation to the cube root of x, then a better approximation is given by the value
+
+(x/y^2 +2y)/3
+
+Use this formula to implement a cube-root procedure analogous to the square-root procedure. (In 1.3.4 we will see how to implement Newton’s method in general as an abstraction of these square-root and cube-root procedures.
+
 (define (square x)
   (* x x))
 (define (cube x)
@@ -5,14 +11,11 @@
 (define (good-enough? previous-guess guess)
   (< (abs (/ (- guess previous-guess) guess)) 0.000000000000001))
 
-
 (define (improve guess x) 
   (/ (+ (/ x 
          (square guess)) 
        (* guess 2)) 
      3))
-
-
 
 (define (sqrt-iter guess x)
   (if (good-enough? guess (improve guess x))
